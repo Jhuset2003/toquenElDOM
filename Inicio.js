57fdae832a99cb51c76aa76c0d15f3cdb511dfb4
Jhuset2003 <97614491+Jhuset2003@users.noreply.github.com>
2022-01-26 10:31:14 -0500

    Añadiendo inicio a la main part 1

---
+++ Inicio.js
@@ -0,0 +1,8 @@
+/*menu */
+let btn_menu = document.querySelector(".btn-menu");
+let menu_content = document.querySelector(".menu-content");
+
+btn_menu.addEventListener("click",()=>{
+        menu_content.classList.toggle("active");
+    })
+/*menu*/
